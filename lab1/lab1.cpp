371b6c12d1cf8345718721fef20237904c16cabc
armin <armin.fazlic@gmail.com>
2016-08-24 18:24:20 +0000

    ugly thing

---
+++ lab1/lab1.cpp
@@ -0,0 +1,13 @@
+// make art
+//Armin Fazlic
+//08/24/16
+#include <iostream>
+using namespace std;
+int main ()
+{
+cout << " |\\_/|" << endl;
+cout << "/ @ @ \\" << endl;
+cout << "( > * < )" << endl;
+cout << "' >>x<< '" << endl;
+cout <<"/    0    \\" << endl;
+}
