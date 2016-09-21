5ef2f9bc6ac54b2f0e1bb3d2a4cd108b798b3387
armin <armin.fazlic@gmail.com>
2016-09-21 03:32:26 +0000

    lab7 submission

---
+++ lab7/lab7.cpp
@@ -0,0 +1,79 @@
+//armin fazlic
+//Making a song and such
+// function input into a song 
+
+
+#include <iostream>
+using namespace std;
+
+int chorus(int y) {
+
+if (y == 1){
+cout << "And I find it kinda funny \n"
+"I find it kinda sad \n"
+"The dreams in which I'm dying \n"
+"Are the best I've ever had \n"
+"I find it hard to tell you \n"
+"I find it hard to take \n"
+"When people run in circles \n"
+"It's a very, very mad world, mad world \n";
+}
+else {
+cout << "And I find it kinda funny \n"
+"I look it kinda sad \n"
+"The dreams in which I'm dying \n"
+"Are the best I've ever had \n"
+"I look it hard to tell you \n"
+"I look it hard to take \n"
+"When people run in circles \n"
+"It's a very, very mad world, mad world \n";
+return 0;
+}
+}
+
+using namespace std;
+int main() 
+{
+int x = 1;
+
+cout <<
+"All around me are familiar faces \n"
+"Worn out places, worn out faces \n"
+"Bright and early for their daily races \n"
+"Going nowhere, going nowhere \n"
+"\n"
+"Their tears are filling up their glasses \n"
+"No expression, no expression \n"
+"Hide my head, I wanna drown my sorrow \n"
+"No tomorrow, no tomorrow \n"
+"\n";
+chorus(x);
+cout << "\n"
+ "Children waiting for the day they feel good \n"
+"Happy Birthday, Happy Birthday \n"
+"And I feel the way that every child should \n"
+"Sit and listen, sit and listen \n"
+"\n"
+"Went to school and I was very nervous \n"
+"No one knew me, no one knew me \n"
+"Hello teacher tell me what's my lesson \n"
+"Look right through me, look right through me \n"
+"\n";
+chorus(x+1);
+cout << "\n"
+"Enlarging your world \n"
+"Mad world \n";
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+} 
